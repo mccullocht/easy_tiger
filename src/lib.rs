c9888873dc79ad47afda01a51fa6e3ece42d5f73
Trevor McCulloch <mccullocht@gmail.com>
2024-11-01 10:34:58 -0700

    Use Cow Record

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-use std::num::NonZero;
-
 use quantization::binary_quantize;
-use wt_mdb::{RecordView, Session};
+use wt_mdb::{Record, Session};
 
 mod input;
 mod quantization;
@@ -14,16 +12,11 @@ pub fn binary_quantize_and_load<'a, V>(
 where
     V: Iterator<Item = &'a [f32]>,
 {
-    // XXX maybe I can make Record contain a Cow?
-    // you could have RecordView<'a> and type Record = RecordView<'static>
-    // * RecordView::from_slice(s: &'a [u8]) -> RecordView<'a>
-    // * RecordView::from_vec(v: Vec<u8>) -> Record
     session.bulk_load(
         table_name,
         None,
-        vectors.enumerate().map(|(i, v)| {
-            let b = binary_quantize(v);
-            RecordView::new(i as i64, &b)
-        }),
+        vectors
+            .enumerate()
+            .map(|(i, v)| Record::new(i as i64, binary_quantize(v))),
     )
 }

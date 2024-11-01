27c0197a18784b13ea4974784441de030a7cff47
Trevor McCulloch <mccullocht@gmail.com>
2024-10-31 22:25:30 -0700

    slight problem with RecordView and iterator lambdas

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::num::NonZero;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use quantization::binary_quantize;
+use wt_mdb::{RecordView, Session};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+mod input;
+mod quantization;
+
+pub fn binary_quantize_and_load<'a, V>(
+    session: &Session,
+    table_name: &str,
+    vectors: V,
+) -> wt_mdb::Result<()>
+where
+    V: Iterator<Item = &'a [f32]>,
+{
+    // XXX maybe I can make Record contain a Cow?
+    // you could have RecordView<'a> and type Record = RecordView<'static>
+    // * RecordView::from_slice(s: &'a [u8]) -> RecordView<'a>
+    // * RecordView::from_vec(v: Vec<u8>) -> Record
+    session.bulk_load(
+        table_name,
+        None,
+        vectors.enumerate().map(|(i, v)| {
+            let b = binary_quantize(v);
+            RecordView::new(i as i64, &b)
+        }),
+    )
 }

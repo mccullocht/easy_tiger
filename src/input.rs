27116e173ba59eb86db3e4a7c842c4641c11e0d4
Trevor McCulloch <trevor.mcculloch@mongodb.com>
2024-11-25 13:29:44 -0800

    deref vector store

--- src/input.rs
+++ src/input.rs
@@ -1,6 +1,99 @@
-use std::{io, num::NonZero, ops::Index};
+use std::{
+    io,
+    num::NonZero,
+    ops::{Deref, Index},
+};
 
 use stable_deref_trait::StableDeref;
+
+/// A store of vector data indexed by a densely assigned range of values.
+pub trait VectorStore: Index<usize> {
+    type Elem;
+
+    /// Return the number of vectors in the store.
+    fn len(&self) -> usize;
+
+    /// Return true if this store is empty.
+    fn is_empty(&self) -> bool;
+
+    /// Return an iterator over all the vectors in the store.
+    fn iter(&self) -> impl Iterator<Item = &[Self::Elem]>;
+}
+
+pub struct DerefVectorStore<E: 'static, D> {
+    // NB: the contents of data is referenced by raw_vectors.
+    #[allow(dead_code)]
+    data: D,
+    raw_vectors: &'static [E],
+
+    stride: usize,
+    len: usize,
+}
+
+impl<E, D> DerefVectorStore<E, D>
+where
+    D: Deref<Target = [u8]>,
+{
+    pub fn new(data: D, stride: NonZero<usize>) -> io::Result<Self> {
+        let elem_width = std::mem::size_of::<E>();
+        let vectorp = data.as_ptr() as *const E;
+        if !vectorp.is_aligned() {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!(
+                    "input vector data not aligned to element width {}",
+                    elem_width
+                ),
+            ));
+        }
+        if data.len() % (elem_width * stride.get()) != 0 {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                format!(
+                    "input vector data does not divide evenly into stride byte length of {}",
+                    elem_width * stride.get()
+                ),
+            ));
+        }
+        let len = data.len() / stride.get();
+
+        // Safety: StableDeref guarantees the pointer is stable even after a move.
+        let raw_vectors: &'static [E] =
+            unsafe { std::slice::from_raw_parts(vectorp, data.len() / elem_width) };
+        Ok(Self {
+            data,
+            raw_vectors,
+            stride: stride.get(),
+            len,
+        })
+    }
+}
+
+impl<E, D> VectorStore for DerefVectorStore<E, D> {
+    type Elem = E;
+
+    fn len(&self) -> usize {
+        self.len
+    }
+
+    fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    fn iter(&self) -> impl Iterator<Item = &[Self::Elem]> {
+        self.raw_vectors.chunks(self.stride)
+    }
+}
+
+impl<E, D> Index<usize> for DerefVectorStore<E, D> {
+    type Output = [E];
+
+    fn index(&self, index: usize) -> &Self::Output {
+        let start = index * self.stride;
+        let end = start + self.stride;
+        &self.raw_vectors[start..end]
+    }
+}
 
 /// Immutable store for numpy formatted f32 vectors.
 ///

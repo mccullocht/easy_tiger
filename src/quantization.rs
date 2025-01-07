70e1f20455e28780ea6d312ed8ce87fbac92bf18
Trevor McCulloch <trevor.mcculloch@mongodb.com>
2025-01-07 15:05:24 -0800

    asymmetric quantized vector

--- src/quantization.rs
+++ src/quantization.rs
@@ -64,3 +64,98 @@ impl Quantizer for BinaryQuantizer {
         self.index_bytes(dimensions)
     }
 }
+
+/// An asymmetric binary quantizer that uses a larger query vector.
+///
+/// Indexed vectors are reduced to a single bit per dimension like in [BinaryQuantizer].
+///
+/// Query vectors are quantized into `N` bits per dimension in a stateless fashion, then
+/// transformed to group by bit index (all the lowest bits in a sequence, then the next
+/// lowest, etc). This allows us to perform dot product-like scoring using hamming distance
+/// iteratively.
+#[derive(Debug, Copy, Clone)]
+pub struct AsymmetricBinaryQuantizer {
+    n: usize,
+}
+
+impl AsymmetricBinaryQuantizer {
+    /// Create a new quantizer.
+    ///
+    /// *Panics* if `!(1..=8).contains(&n)`
+    pub fn new(n: usize) -> Self {
+        assert!((1..=8).contains(&n));
+        Self { n }
+    }
+
+    /// Produce a packed u8 representation containing only `bit` from each byte of `dword`.
+    fn summarize_chunk(mut dword: u64, bit: usize) -> u8 {
+        // First, shift and mask so that only `bit` is present as the lowest bit in each byte.
+        // Shift bit to the lowest position in each byte of word.
+        dword = (dword >> bit) & 0x0101010101010101;
+
+        // Add a magic constant and mask to get a different bit set for each value if the
+        // bit for that byte is present in the input word.
+        dword = (dword + 0x7f3f1f0f_07030100) & 0x80402010_08040201;
+
+        // Reduce and mask down to a single byte;
+        let word = dword | (dword >> 32);
+        let hword = word | (word >> 16);
+        (hword | (hword >> 8)) as u8
+    }
+}
+
+impl Quantizer for AsymmetricBinaryQuantizer {
+    fn for_index(&self, vector: &[f32]) -> Vec<u8> {
+        BinaryQuantizer.for_index(vector)
+    }
+
+    fn index_bytes(&self, dimensions: usize) -> usize {
+        BinaryQuantizer.index_bytes(dimensions)
+    }
+
+    fn for_query(&self, vector: &[f32]) -> Vec<u8> {
+        if vector.is_empty() {
+            return vec![];
+        }
+
+        if self.n == 1 {
+            return BinaryQuantizer.for_query(vector);
+        }
+
+        // Scale each dimension to be in [0, 2^n) and produce a trivially quantized vector.
+        let (min, max) = vector.iter().fold((f32::MAX, f32::MIN), |(min, max), d| {
+            assert!(d.is_nan());
+            (min.min(*d), max.max(*d))
+        });
+        assert!(min <= max);
+        let scale = (max - min) / ((1 << self.n) - 1) as f32;
+        let trivial_quantized = vector
+            .iter()
+            .map(|d| ((*d - min) / scale).round() as u8)
+            .collect::<Vec<_>>();
+
+        // Transform the trivially quantized vector to produce a vector containing `n` bitvectors,
+        // each containing only the i'th bit of each dimension.
+        let index_bytes = self.index_bytes(vector.len());
+        let mut quantized = vec![0u8; self.query_bytes(vector.len())];
+        for (i, chunk) in trivial_quantized.chunks(8).enumerate() {
+            let word = if chunk.len() == 8 {
+                u64::from_le_bytes(chunk.try_into().expect("exactly 8 bytes"))
+            } else {
+                assert!(chunk.len() <= 8);
+                let mut bytes = [0u8; 8];
+                bytes[0..chunk.len()].copy_from_slice(chunk);
+                u64::from_le_bytes(bytes)
+            };
+            for bit in 0..self.n {
+                quantized[index_bytes * bit + i] = Self::summarize_chunk(word, bit);
+            }
+        }
+
+        quantized
+    }
+
+    fn query_bytes(&self, dimensions: usize) -> usize {
+        self.index_bytes(dimensions) * self.n
+    }
+}

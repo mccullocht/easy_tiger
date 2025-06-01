f85923a2bb406b31a703b1d4a8b566ea172f1e26
Trevor McCulloch <mccullocht@gmail.com>
2025-06-01 10:34:22 -0700

    searcher command. panics

--- et/src/spann.rs
+++ et/src/spann.rs
@@ -1,9 +1,11 @@
 mod bulk_load;
+mod search;
 
 use std::{io, sync::Arc};
 
 use bulk_load::{bulk_load, BulkLoadArgs};
 use clap::{Args, Subcommand};
+use search::{search, SearchArgs};
 use wt_mdb::Connection;
 
 #[derive(Args)]
@@ -16,6 +18,8 @@ pub struct SpannArgs {
 pub enum Command {
     /// Bulk load a set of vectors into an empty SPANN-ish index.
     BulkLoad(BulkLoadArgs),
+    /// Search a SPANN-ish index.
+    Search(SearchArgs),
 }
 
 pub fn spann_command(
@@ -25,5 +29,6 @@ pub fn spann_command(
 ) -> io::Result<()> {
     match args.command {
         Command::BulkLoad(args) => bulk_load(connection, index_name, args),
+        Command::Search(args) => search(connection, index_name, args),
     }
 }
---
+++ et/src/spann/search.rs
@@ -0,0 +1,314 @@
+// XXX factor out common components with graph search.
+
+use rayon::prelude::*;
+
+use std::{
+    fs::File,
+    io::{self},
+    num::NonZero,
+    ops::Add,
+    path::PathBuf,
+    sync::Arc,
+    time::{Duration, Instant},
+};
+
+use clap::Args;
+use easy_tiger::{
+    graph::GraphSearchParams,
+    input::{DerefVectorStore, VectorStore},
+    spann::{SessionIndexReader, SpannSearchParams, SpannSearcher, TableIndex},
+    Neighbor,
+};
+use memmap2::Mmap;
+use wt_mdb::{Connection, Result, Session};
+use wt_sys::{WT_STAT_CONN_CURSOR_SEARCH, WT_STAT_CONN_READ_IO};
+
+use crate::ui::progress_bar;
+
+#[derive(Args)]
+pub struct SearchArgs {
+    /// Path to numpy formatted little-endian float vectors.
+    #[arg(short, long)]
+    query_vectors: PathBuf,
+    /// Number head (centroid) candidates in the search list.
+    #[arg(long)]
+    head_candidates: NonZero<usize>,
+    /// Number of head (centroid) results to re-rank at the end of each search.
+    /// If unset, use the same figure as --head-candidates.
+    #[arg(long)]
+    head_rerank_budget: Option<usize>,
+    /// Number of centroids to search postings of. Must be >= than --head-rerank-budget
+    #[arg(long)]
+    posting_centroids: NonZero<usize>,
+    /// Number of posting list candidates to keep, effectively a limit.
+    #[arg(long)]
+    posting_candidates: NonZero<usize>,
+    /// Number of posting results to keep.
+    /// If unset, use the same figure as --posting-candidates
+    #[arg(long)]
+    posting_rerank_budget: Option<usize>,
+    /// Maximum number of queries to run. If unset, run all queries in the vector file.
+    #[arg(short, long)]
+    limit: Option<usize>,
+
+    /// Path buf to numpy u32 formatted neighbors file.
+    /// This should include one row of length neighbors_len for each vector in query_vectors.
+    #[arg(long)]
+    neighbors: Option<PathBuf>,
+    /// Number of neighbors for each query in the neighbors file.
+    #[arg(long, default_value = "100")]
+    neighbors_len: NonZero<usize>,
+    /// Compute recall@k. Must be <= neighbors_len.
+    #[arg(long)]
+    recall_k: Option<NonZero<usize>>,
+
+    #[arg(long, default_value = "1")]
+    warmup_iters: usize,
+    #[arg(long, default_value = "2")]
+    test_iters: usize,
+}
+
+pub fn search(connection: Arc<Connection>, index_name: &str, args: SearchArgs) -> io::Result<()> {
+    let index = Arc::new(TableIndex::from_db(&connection, index_name)?);
+    let query_vectors = easy_tiger::input::DerefVectorStore::new(
+        unsafe { Mmap::map(&File::open(args.query_vectors)?)? },
+        index.head_config().config().dimensions,
+    )?;
+    let limit = std::cmp::min(
+        query_vectors.len(),
+        args.limit.unwrap_or(query_vectors.len()),
+    );
+    let search_params = SpannSearchParams {
+        head_params: GraphSearchParams {
+            beam_width: args.head_candidates,
+            num_rerank: args
+                .head_rerank_budget
+                .unwrap_or(args.head_candidates.get()),
+        },
+        num_centroids: args.posting_centroids,
+        limit: args.posting_candidates,
+        num_rerank: args
+            .posting_rerank_budget
+            .unwrap_or(args.posting_candidates.get()),
+    };
+    let recall_computer = if let Some((neighbors, recall_k)) = args.neighbors.zip(args.recall_k) {
+        let neighbors = DerefVectorStore::<u32, _>::new(
+            unsafe { Mmap::map(&File::open(neighbors)?)? },
+            args.neighbors_len,
+        )?;
+        if neighbors.len() != query_vectors.len() {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                format!(
+                    "neighbors must have the same number of rows as query_vectors ({} vs {})",
+                    neighbors.len(),
+                    query_vectors.len()
+                ),
+            ));
+        }
+        Some(RecallComputer::new(recall_k, neighbors)?)
+    } else {
+        None
+    };
+
+    if args.warmup_iters > 0 {
+        search_phase(
+            "warmup",
+            args.warmup_iters,
+            limit,
+            &query_vectors,
+            &index,
+            &connection,
+            search_params,
+            recall_computer.as_ref(),
+        )?;
+    }
+
+    if args.test_iters > 0 {
+        let stats = search_phase(
+            "test",
+            args.test_iters,
+            limit,
+            &query_vectors,
+            &index,
+            &connection,
+            search_params,
+            recall_computer.as_ref(),
+        )?;
+
+        println!(
+            "queries {} avg duration {:0.6}s max duration {:0.6}s",
+            stats.count,
+            stats.total_duration.as_secs_f64() / stats.count as f64,
+            stats.max_duration.as_secs_f64(),
+        );
+
+        // TODO: reset connection stats so this is more accurate.
+        let (search_calls, read_io) = cache_hit_stats(&connection.open_session()?)?;
+        println!(
+            "cache hit rate {:.2}% ({} reads, {} lookups)",
+            (search_calls - read_io) as f64 * 100.0 / search_calls as f64,
+            read_io,
+            search_calls,
+        );
+
+        if let Some((computer, recalled_count)) = recall_computer.zip(stats.total_recall_results) {
+            println!(
+                "recall@{} {:0.6}",
+                computer.k,
+                recalled_count as f64 / (stats.count * computer.k) as f64
+            );
+        }
+    }
+
+    Ok(())
+}
+
+fn search_phase<Q: Send + Sync, N: Send + Sync>(
+    name: &'static str,
+    iters: usize,
+    limit: usize,
+    query_vectors: &DerefVectorStore<f32, Q>,
+    index: &Arc<TableIndex>,
+    connection: &Arc<Connection>,
+    search_params: SpannSearchParams,
+    recall_computer: Option<&RecallComputer<DerefVectorStore<u32, N>>>,
+) -> io::Result<AggregateSearchStats> {
+    let query_indices = (0..limit)
+        .into_iter()
+        .cycle()
+        .take(iters * limit)
+        .collect::<Vec<_>>();
+    let progress = progress_bar(query_indices.len(), Some(name));
+    let stats = query_indices
+        .into_par_iter()
+        .map_init(
+            || SearcherState::new(&index, &connection, search_params).unwrap(),
+            |searcher, index| {
+                let stats = searcher.query(index, &query_vectors[index], recall_computer);
+                progress.inc(1);
+                stats
+            },
+        )
+        .try_reduce(|| AggregateSearchStats::default(), |a, b| Ok(a + b))?;
+    progress.finish_using_style();
+    Ok(stats)
+}
+
+struct SearcherState {
+    reader: SessionIndexReader,
+    searcher: SpannSearcher,
+}
+
+impl SearcherState {
+    fn new(
+        index: &Arc<TableIndex>,
+        connection: &Arc<Connection>,
+        search_params: SpannSearchParams,
+    ) -> io::Result<Self> {
+        Ok(Self {
+            reader: SessionIndexReader::new(&index, connection.open_session()?),
+            searcher: SpannSearcher::new(search_params),
+        })
+    }
+
+    fn query<N: VectorStore<Elem = u32>>(
+        &mut self,
+        index: usize,
+        query: &[f32],
+        recall_computer: Option<&RecallComputer<N>>,
+    ) -> io::Result<AggregateSearchStats> {
+        self.reader.session().begin_transaction(None)?;
+        let start = Instant::now();
+        let results = self.searcher.search(query, &mut self.reader)?;
+        let duration = Instant::now() - start;
+        self.reader.session().rollback_transaction(None)?;
+        Ok(AggregateSearchStats::new(
+            duration,
+            recall_computer.map(|r| r.compute_recall(index, &results)),
+        ))
+    }
+}
+
+pub struct RecallComputer<N> {
+    k: usize,
+    neighbors: N,
+}
+
+impl<N> RecallComputer<N>
+where
+    N: VectorStore<Elem = u32>,
+{
+    fn new(k: NonZero<usize>, neighbors: N) -> io::Result<Self> {
+        if k.get() <= neighbors.elem_stride() {
+            Ok(Self {
+                k: k.get(),
+                neighbors,
+            })
+        } else {
+            Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "recall k must be <= neighbors_len",
+            ))
+        }
+    }
+
+    fn compute_recall(&self, query_index: usize, query_results: &[Neighbor]) -> usize {
+        let mut expected = self.neighbors[query_index][..self.k].to_vec();
+        expected.sort();
+        query_results
+            .iter()
+            .take(self.k)
+            .filter(|n| expected.binary_search(&(n.vertex() as u32)).is_ok())
+            .count()
+    }
+}
+
+#[derive(Default)]
+struct AggregateSearchStats {
+    count: usize,
+    total_duration: Duration,
+    max_duration: Duration,
+    total_recall_results: Option<usize>,
+}
+
+impl AggregateSearchStats {
+    fn new(duration: Duration, recall: Option<usize>) -> Self {
+        Self {
+            count: 1,
+            total_duration: duration,
+            max_duration: duration,
+            total_recall_results: recall,
+        }
+    }
+}
+
+impl Add<AggregateSearchStats> for AggregateSearchStats {
+    type Output = AggregateSearchStats;
+
+    fn add(self, rhs: AggregateSearchStats) -> Self::Output {
+        Self {
+            count: self.count + rhs.count,
+            total_duration: self.total_duration + rhs.total_duration,
+            max_duration: std::cmp::max(self.max_duration, rhs.max_duration),
+            total_recall_results: self
+                .total_recall_results
+                .zip(rhs.total_recall_results)
+                .map(|(a, b)| a + b)
+                .or(self.total_recall_results)
+                .or(rhs.total_recall_results),
+        }
+    }
+}
+
+/// Count lookup calls and read IOs. This can be used to estimate cache hit rate.
+fn cache_hit_stats(session: &Session) -> Result<(i64, i64)> {
+    let mut stat_cursor = session.new_stats_cursor(wt_mdb::options::Statistics::Fast, None)?;
+    let search_calls = stat_cursor
+        .seek_exact(WT_STAT_CONN_CURSOR_SEARCH)
+        .expect("WT_STAT_CONN_CURSOR_SEARCH")?;
+    let read_ios = stat_cursor
+        .seek_exact(WT_STAT_CONN_READ_IO)
+        .expect("WT_STAT_CONN_READ_IO")?;
+    Ok((search_calls, read_ios))
+}

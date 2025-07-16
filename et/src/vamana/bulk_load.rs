4870eb3db34cbbc5261f83ff7c340f405a24c72e
Trevor McCulloch <mccullocht@gmail.com>
2025-07-16 16:00:17 -0700

    fix vamana bulk load

--- et/src/vamana/bulk_load.rs
+++ et/src/vamana/bulk_load.rs
@@ -35,11 +35,7 @@ pub struct BulkLoadArgs {
     ///
     /// `split` puts raw vectors, nav vectors, and graph edges each in separate tables. If results
     /// are being re-ranked this will require additional reads to complete.
-    ///
-    /// `raw_vector_in_graph` places raw vectors and graph edges in the same table. When a vertex
-    /// is visited the raw vector is read and saved for re-scoring. This minimizes the number of
-    /// reads performed and is likely better for indices with less traffic.
-    #[arg(long, value_enum, default_value = "raw_vector_in_graph")]
+    #[arg(long, value_enum, default_value = "split")]
     layout: GraphLayout,
     /// If true, load all quantized vectors into a trivial memory store for bulk loading.
     /// This can be significantly faster than reading these values from WiredTiger.

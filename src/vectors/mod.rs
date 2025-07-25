d0651ca50958a59abf17e0921c755358657b6fb3
Trevor McCulloch <mccullocht@gmail.com>
2025-07-25 16:06:00 -0700

    refactor

--- src/vectors/mod.rs
+++ src/vectors/mod.rs
@@ -12,13 +12,51 @@ use crate::{
 
 mod raw;
 
-// XXX so we are able to create a formatter that is run on ingress and can be used for both the
-// nav vector and raw vector tables, which also makes the raw vector table _optional_.
+// XXX immediate TODOs
+// * invert relationship between Quantizer and F32VectorCoding.
 
-// XXX i don't want the raw format to dictate the scorer we use because that would force scoring
-// to be symmetric and that is not great for us. for a given table we know both the similarity
-// and the format so we can create a symmetric implementation but we might need more information
-// to create an asymmetric implementation.
+// XXX some changes that need to happen:
+// * in bulk loading we should always use the WT table because it represent a transform.
+// * mod quantization goes away entirely, including factory functions.
+// * mod query_distance and mod distance also go away. factory functions move here.
+
+// XXX docos
+pub enum F32VectorCoding {
+    /// Little-endian f32 values encoded as bytes.
+    Raw,
+    /// Little-endian f32 values encoded as bytes, but l2 normalized first.
+    /// The resulting unit vectors can be used to cheaply compute angular distance.
+    RawL2Normalized,
+    /// Single bit (sign bit) per dimension.
+    BinaryQuantized,
+    /// Quantize to n-bits per dimension, but format to facilitate hamming distance calculations.
+    NBitBinaryQuantized(usize),
+    /// Normalize and quantize into an i8 value.
+    /// This normalizes the input vector but otherwise does not shape quantization to the input.
+    I8NaiveQuantized,
+    /// Normalize and quantize into an i8 value, shaped to the input vector.
+    /// This uses the contents of the vector to try to reduce quantization error.
+    I8ScaledUniform,
+}
+
+impl F32VectorCoding {
+    /// Create a new coder for this format.
+    pub fn new_coder(&self) -> Box<dyn F32VectorCoder> {
+        match self {
+            Self::Raw => Box::new(RawF32VectorCoder),
+            Self::RawL2Normalized => Box::new(RawL2NormalizedF32VectorCoder),
+            Self::BinaryQuantized => Box::new(QuantizedVectorCoding(BinaryQuantizer)),
+            Self::NBitBinaryQuantized(n) => {
+                Box::new(NBitBinaryQuantized(AsymmetricBinaryQuantizer::new(*n)))
+            }
+            Self::I8NaiveQuantized => Box::new(QuantizedVectorCoding(I8NaiveQuantizer)),
+            Self::I8ScaledUniform => Box::new(QuantizedVectorCoding(I8ScaledUniformQuantizer)),
+        }
+    }
+
+    // XXX we can do symmetrical distance by using the same F32VectorCoding on both sides.
+    // XXX we can also do asymmetrical for some combinations
+}
 
 // XXX docos
 pub trait F32VectorCoder: Send + Sync {
@@ -38,46 +76,7 @@ pub trait F32VectorCoder: Send + Sync {
     fn byte_len(&self, dimensions: usize) -> usize;
 }
 
-// XXX should there be a raw normalized format??? it's not really an alternate format but rather
-// a processing step.
-pub enum F32VectorCoding {
-    /// Little-endian f32 values encoded as bytes.
-    Raw,
-    /// Little-endian f32 values encoded as bytes, but l2 normalized first.
-    /// This loses vector magnitude but the result will always be suitable for angular distance.
-    RawL2Normalized,
-    /// Single bit (sign bit) per dimension.
-    BinaryQuantized,
-    /// Quantize to n-bits per dimension, but format to facilitate hamming distance calculations.
-    NBitBinaryQuantized(usize),
-    /// Normalize and quantize into an i8 value.
-    /// This normalizes the input vector but otherwise does not shape quantization to the input.
-    I8NaiveQuantized,
-    /// Normalize and quantize into an i8 value, shaped to the input vector.
-    /// This uses the contents of the vector to try to reduce quantization error.
-    I8ScaledUniform,
-}
-
-impl F32VectorCoding {
-    // XXX should this accept the VectorSimilarity? it would allow me to normalize.
-    pub fn new_coder(&self) -> Box<dyn F32VectorCoder> {
-        match self {
-            Self::Raw => Box::new(RawF32VectorCoder),
-            Self::RawL2Normalized => Box::new(RawL2NormalizedF32VectorCoder),
-            Self::BinaryQuantized => Box::new(QuantizedVectorCoding(BinaryQuantizer)),
-            Self::NBitBinaryQuantized(n) => {
-                Box::new(NBitBinaryQuantized(AsymmetricBinaryQuantizer::new(*n)))
-            }
-            Self::I8NaiveQuantized => Box::new(QuantizedVectorCoding(I8NaiveQuantizer)),
-            Self::I8ScaledUniform => Box::new(QuantizedVectorCoding(I8ScaledUniformQuantizer)),
-        }
-    }
-
-    // XXX we can do symmetrical distance by using the same F32VectorCoding on both sides.
-    // XXX we can also do asymmetrical for some combinations
-}
-
-// TODO: remove this entirely.
+// XXX remove this entirely, write native coders for each format.
 #[derive(Debug, Copy, Clone)]
 struct QuantizedVectorCoding<Q: Debug + Copy + Clone>(Q);
 
@@ -95,7 +94,7 @@ impl<Q: Quantizer + Debug + Copy + Clone> F32VectorCoder for QuantizedVectorCodi
     }
 }
 
-// TODO: remove this entirely.
+// XXX remove this entirely, write a native coder for this format.
 #[derive(Debug, Copy, Clone)]
 struct NBitBinaryQuantized(AsymmetricBinaryQuantizer);
 

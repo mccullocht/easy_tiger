b6144f540eeecb08bd952e1df1de81cabbf058e9
Trevor McCulloch <mccullocht@gmail.com>
2025-08-09 21:16:08 -0700

    do tail split

--- src/vectors/binary.rs
+++ src/vectors/binary.rs
@@ -35,6 +35,9 @@ impl VectorDistance for HammingDistance {
 
 /// Quantize the input vector into an i8 vector and compare it to a binary quantized vector.
 ///
+/// This is ~7x slower than raw hamming distance computation, which is less than it would be to use
+/// a transposed bitmap and compute hamming distance 8x.
+///
 /// This assumes vectors are l2 normalized like for dot product distance.
 #[derive(Debug, Clone)]
 pub struct I1DotProductQueryDistance {
@@ -71,11 +74,11 @@ impl I1DotProductQueryDistance {
     }
 
     #[allow(dead_code)]
-    fn dot_unnormalized_i32_scalar(&self, vector: &[u8]) -> i32 {
+    fn dot_unnormalized_i32_scalar(&self, vector: &[u8], start_dim: usize) -> i32 {
         let decode_table = [-self.i1_value, self.i1_value];
-        self.quantized
+        self.quantized[start_dim..]
             .chunks(8)
-            .zip(vector.iter())
+            .zip(vector[(start_dim / 8)..].iter())
             .flat_map(|(q, d)| {
                 q.iter()
                     .zip(
@@ -99,54 +102,29 @@ impl I1DotProductQueryDistance {
 
     #[cfg(not(target_arch = "aarch64"))]
     fn dot_unnormalized_i32(&self, vector: &[u8]) -> i32 {
-        self.dot_unnormalized_i32_scalar(vector)
+        self.dot_unnormalized_i32_scalar(vector, 0)
     }
 
-    // XXX this is ~7x slower than raw hamming.
     #[cfg(target_arch = "aarch64")]
     fn dot_unnormalized_i32(&self, vector: &[u8]) -> i32 {
-        // XXX fix tail split
-        unsafe {
+        let tail_split = self.quantized.len() & !15;
+        let partial_dot = unsafe {
             use std::arch::aarch64::{
-                vaddq_s32, vaddvq_s32, vandq_u8, vcombine_u8, vdup_n_u8, vdupq_n_s16, vdupq_n_s32,
-                vdupq_n_u8, vget_low_s8, vld1q_s8, vld1q_u8, vminq_u8, vmlal_high_s8, vmlal_s8,
-                vpaddlq_s16, vqtbl1q_s8,
+                vaddq_s32, vaddvq_s32, vandq_u8, vcombine_u8, vdup_n_u8, vdupq_n_s32, vdupq_n_u8,
+                vget_low_s8, vld1q_s8, vld1q_u8, vminq_u8, vmlal_high_s8, vmull_s8, vpaddlq_s16,
+                vqtbl1q_s8,
             };
 
-            let doc_mask = vld1q_u8(
-                [
-                    0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x01, 0x02, 0x04, 0x08, 0x10,
-                    0x20, 0x40, 0x80,
-                ]
-                .as_ptr(),
-            );
-            let doc_tbl_mask = vld1q_s8(
-                [
-                    -self.i1_value,
-                    self.i1_value,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                    0,
-                ]
-                .as_ptr(),
-            );
+            let doc_mask =
+                vld1q_u8([1, 2, 4, 8, 16, 32, 64, 128, 1, 2, 4, 8, 16, 32, 64, 128].as_ptr());
+            let v = self.i1_value;
+            let doc_tbl_mask = vld1q_s8([-v, v, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0].as_ptr());
             let mut dot = vdupq_n_s32(0);
-            for i in (0..self.quantized.len()).step_by(16) {
+            for i in (0..tail_split).step_by(16) {
                 let qv = vld1q_s8(self.quantized.as_ptr().add(i));
                 // Broadcast the next two byte values from doc to two different 8 byte values, then
-                // mask and min to do an ~invert movemask operation. We wil then table decode zeros
-                // into a negative value and 1s into a positive value.
+                // mask for each bit and take min(1) to get 0 or 1 in a lane for each bit. This is
+                // fed to a table decoder to get a representative positive or negative value.
                 let dv = vqtbl1q_s8(
                     doc_tbl_mask,
                     vminq_u8(
@@ -158,17 +136,21 @@ impl I1DotProductQueryDistance {
                     ),
                 );
 
-                // XXX vdot_s32 would almost certainly be faster here (3 instr -> 1)
-                let dot_half = vmlal_high_s8(
-                    vmlal_s8(vdupq_n_s16(0), vget_low_s8(qv), vget_low_s8(dv)),
-                    qv,
-                    dv,
+                // Multiply i8 entries and widen to i16, then sum pairs to widen to i32.
+                // 127 * 127 * 2 < 2^15-1 so this can be done without overflow/underflow.
+                dot = vaddq_s32(
+                    dot,
+                    vpaddlq_s16(vmlal_high_s8(
+                        vmull_s8(vget_low_s8(qv), vget_low_s8(dv)),
+                        qv,
+                        dv,
+                    )),
                 );
-                dot = vaddq_s32(dot, vpaddlq_s16(dot_half));
             }
 
             vaddvq_s32(dot)
-        }
+        };
+        partial_dot + self.dot_unnormalized_i32_scalar(vector, tail_split)
     }
 }
 

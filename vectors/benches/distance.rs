a11582daca086d8f1bf24f027f2f890be8ea228e
Trevor McCulloch <mccullocht@gmail.com>
2025-10-16 19:39:15 -0700

    benches

--- vectors/benches/distance.rs
+++ vectors/benches/distance.rs
@@ -1,6 +1,6 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use easy_tiger::vectors::{new_query_vector_distance_f32, F32VectorCoding, VectorSimilarity};
+use criterion::{Criterion, criterion_group, criterion_main};
 use rand::{Rng, SeedableRng};
+use vectors::{F32VectorCoding, VectorSimilarity, new_query_vector_distance_f32};
 
 fn generate_test_vectors(dim: usize) -> (Vec<f32>, Vec<f32>) {
     let mut rng = rand_xoshiro::Xoroshiro128PlusPlus::seed_from_u64(0x455A_5469676572);

dd12a597fe18141ec8618dba79f7606c6b279eb7
Trevor McCulloch <mccullocht@gmail.com>
2026-07-01 20:58:36 -0700

    modify_unsafe

--- wt_sys/src/lib.rs
+++ wt_sys/src/lib.rs
@@ -11,3 +11,38 @@
 #![allow(rustdoc::broken_intra_doc_links)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+impl Default for __wt_item {
+    fn default() -> Self {
+        Self {
+            data: std::ptr::null(),
+            size: 0,
+            mem: std::ptr::null_mut(),
+            memsize: 0,
+            flags: 0,
+        }
+    }
+}
+
+impl __wt_item {
+    /// Construct a `WT_ITEM` pointing into `s`. The caller must ensure `s` outlives the item.
+    pub unsafe fn from_slice(s: &[u8]) -> Self {
+        Self {
+            data: s.as_ptr() as *const _,
+            size: s.len(),
+            mem: std::ptr::null_mut(),
+            memsize: 0,
+            flags: 0,
+        }
+    }
+}
+
+impl Default for __wt_modify {
+    fn default() -> Self {
+        Self {
+            data: __wt_item::default(),
+            offset: 0,
+            size: 0,
+        }
+    }
+}

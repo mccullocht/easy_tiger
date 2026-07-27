44fc5f3b1d796fb805125d8f8db3e06528b96bca
Trevor McCulloch <mccullocht@gmail.com>
2026-07-27 13:21:30 -0700

    much improved symmetric performance

--- vectors/src/quiver.rs
+++ vectors/src/quiver.rs
@@ -111,42 +111,44 @@ impl F32VectorCoder for Coder {
     }
 }
 
-/// Compares the low 2 bits of a and b to produce a distance.
-const fn distance(a: u8, b: u8) -> i8 {
-    let h = (a & 3) ^ (b & 3);
-    let r: i8 = if h & 1 == 0 {
-        // Agree on magnitude
-        if a & 1 == 1 {
-            4 // strong
-        } else {
-            1 // weak
-        }
-    } else {
-        // Disagree on magnitude
-        2
-    };
-    // If the signs differ the result is negative.
-    if h & 2 == 0 {
-        r
-    } else {
-        -r
-    }
+const SGN_MASK: u128 = 0x5555_5555_5555_5555_5555_5555_5555_5555;
+const MAG_MASK: u128 = 0xAAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA_AAAA;
+
+/// Take 256 bits of interleaved (sign,magnitude) input and generate two 128 bit outputs that
+/// contain all of the signs and all of the magnitudes in consistent order.
+#[inline]
+fn bitplane_split256(v: [u8; 32]) -> (u128, u128) {
+    let parts = v.as_chunks::<16>().0;
+    let a = u128::from_le_bytes(parts[0]);
+    let b = u128::from_le_bytes(parts[1]);
+
+    // Dims from 'a' are in even bits; dims from 'b' are in odd bits.
+    let sgn = (a & SGN_MASK) | ((b & SGN_MASK) << 1);
+    let mag = ((a & MAG_MASK) >> 1) | (b & MAG_MASK);
+    (sgn, mag)
 }
 
-/// Precompute a table of distances where the low nibble is 2 dimensions from one vector and the
-/// high nibble is 2 dimensions from the other vector.
-const fn distance_table() -> [i8; 256] {
-    let mut table = [0i8; 256];
-    let mut i = 0;
-    while i < table.len() {
-        let code = i as u8;
-        table[i] = distance(code, code >> 4) + distance(code >> 2, code >> 6);
-        i += 1;
-    }
-    table
-}
+/// Compute raw distance between two vectors with interleaved packed (sign,magnitude) values for
+/// 256 bits of input.
+#[inline]
+fn distance256(a: [u8; 32], b: [u8; 32]) -> i32 {
+    let (a_s, a_m) = bitplane_split256(a);
+    let (b_s, b_m) = bitplane_split256(b);
 
-const DISTANCE_LUT: [i8; 256] = distance_table();
+    let s_x = a_s ^ b_s; // signs mismatch
+    let m_x = a_m ^ b_m; // magnitudes mismatch
+    let m_s = a_m & b_m; // both magnitudes strong
+    let m_w = !(a_m | b_m); // both magnitudes weak
+
+    // Use bitmask combinations + popcnt to count each of our 6 states: (all strong, all weak,
+    // mixed) x (positive, negative).
+    (m_s & !s_x).count_ones() as i32 * 4
+        + (m_s & s_x).count_ones() as i32 * -4
+        + (m_w & !s_x).count_ones() as i32 * 1
+        + (m_w & s_x).count_ones() as i32 * -1
+        + (m_x & !s_x).count_ones() as i32 * 2
+        + (m_x & s_x).count_ones() as i32 * -2
+}
 
 /// Symmetric distance computation for QuiVer vectors.
 ///
@@ -160,14 +162,13 @@ impl VectorDistance for Distance {
         let (query_header, query) = Header::split_and_decode(query);
         let (doc_header, doc) = Header::split_and_decode(doc);
 
+        // XXX need to process tails
         let raw_dist = query
+            .as_chunks::<32>()
+            .0
             .iter()
-            .zip(doc.iter())
-            .map(|(&q, &d)| {
-                let lo_key = ((q & 0xf) | (d << 4)) as usize;
-                let hi_key = ((q >> 4) | (d & 0xf0)) as usize;
-                DISTANCE_LUT[lo_key] as i32 + DISTANCE_LUT[hi_key] as i32
-            })
+            .zip(doc.as_chunks::<32>().0.iter())
+            .map(|(&q, &d)| distance256(q, d))
             .sum::<i32>();
 
         // Use strong count to compute a more accurate denominator for cosine similarity.

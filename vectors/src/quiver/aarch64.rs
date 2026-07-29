3fc363f151fdab4226afcbea7d9a85f99e07aa98
Trevor McCulloch <mccullocht@gmail.com>
2026-07-29 11:37:03 -0700

    faster symmetric quantization by accumulating in vector space

--- vectors/src/quiver/aarch64.rs
+++ vectors/src/quiver/aarch64.rs
@@ -2,8 +2,8 @@ use super::scalar::Scalar;
 use super::Kernel;
 
 use std::arch::aarch64::{
-    int8x16_t, vaddlvq_s8, vandq_s8, vbslq_s8, vcntq_s8, vdupq_n_u8, veorq_s8, vld1q_s8, vmvnq_s8,
-    vorrq_s8, vshlq_n_s8, vshrq_n_s8, vsubq_s8,
+    int8x16_t, vaddlvq_s8, vaddq_s8, vandq_s8, vbslq_s8, vcntq_s8, vdupq_n_s8, vdupq_n_u8,
+    veorq_s8, vld1q_s8, vmvnq_s8, vorrq_s8, vshlq_n_s8, vshrq_n_s8, vsubq_s8,
 };
 
 /// NEON kernel for QuIVer operations.
@@ -30,10 +30,11 @@ impl Neon {
 
 impl Kernel for Neon {
     fn symmetric_distance(a: &[u8], b: &[u8]) -> i32 {
-        // XXX accumulate in SIMD. consider SDOT as part of the strategy.
+        // TODO: consider an SDOT accumulation strategy that is not vulnerable to over or
+        // underflow.
         let (achunks, arem) = a.as_chunks::<32>();
         let (bchunks, brem) = b.as_chunks::<32>();
-        let mut dist = achunks
+        let (w, m, s) = achunks
             .iter()
             .map(Self::bitplane_split256)
             .zip(bchunks.iter().map(Self::bitplane_split256))
@@ -51,12 +52,18 @@ impl Kernel for Neon {
                     let strong =
                         vsubq_s8(vcntq_s8(vandq_s8(m_s, s_m)), vcntq_s8(vandq_s8(m_s, s_x)));
 
-                    vaddlvq_s8(weak) as i32
-                        + vaddlvq_s8(mixed) as i32 * 2
-                        + vaddlvq_s8(strong) as i32 * 4
+                    (weak, mixed, strong)
                 }
             })
-            .sum::<i32>();
+            .fold(
+                // NB: accumulating this way may experience overflow.
+                unsafe { (vdupq_n_s8(0), vdupq_n_s8(0), vdupq_n_s8(0)) },
+                |acc, (w, m, s)| unsafe {
+                    (vaddq_s8(acc.0, w), vaddq_s8(acc.1, m), vaddq_s8(acc.2, s))
+                },
+            );
+        let mut dist =
+            unsafe { vaddlvq_s8(w) as i32 + vaddlvq_s8(m) as i32 * 2 + vaddlvq_s8(s) as i32 * 4 };
         if !arem.is_empty() {
             dist += Scalar::symmetric_distance(arem, brem);
         }

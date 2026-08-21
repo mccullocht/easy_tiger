9f94f26b8b45997524551c7c1aceae46a51c6c6b
Trevor McCulloch <mccullocht@gmail.com>
2026-08-20 21:36:50 -0700

    remove lvq.c

--- vectors/build.rs
+++ vectors/build.rs
@@ -1,8 +1,6 @@
 fn main() {
-    println!("cargo::rerun-if-changed=src/lvq.c");
     println!("cargo::rerun-if-changed=src/quiver.c");
     cc::Build::new()
-        .file("src/lvq.c")
         .file("src/quiver.c")
         .opt_level(3)
         .compile("vectors");
